4bf54a8def6f3d4b98b41fb74e57a7602524347d
Anthony Roussos <github.eloquent385@passinbox.com>
2024-01-20 13:47:15 +0200

    Add Foreign Key to AspNetUsers table

---
+++ CineTicketHub/Data/Migrations/20240120111728_AddForeignKeyToAspNetUsers.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CineTicketHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeyToAspNetUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "user_id",
+                table: "reservations",
+                type: "varchar(255)",
+                nullable: false,
+                collation: "utf8mb4_0900_ai_ci",
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "varchar(21)",
+                maxLength: 21,
+                nullable: false,
+                defaultValue: "",
+                collation: "utf8mb4_0900_ai_ci")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reservations_user_id",
+                table: "reservations",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_reservations_AspNetUsers_user_id",
+                table: "reservations",
+                column: "user_id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_reservations_AspNetUsers_user_id",
+                table: "reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_reservations_user_id",
+                table: "reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "user_id",
+                table: "reservations",
+                type: "longtext",
+                nullable: false,
+                collation: "utf8mb4_0900_ai_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("Relational:Collation", "utf8mb4_0900_ai_ci");
+        }
+    }
+}
--- CineTicketHub/Data/Migrations/CineTicketHubContextModelSnapshot.cs
+++ CineTicketHub/Data/Migrations/CineTicketHubContextModelSnapshot.cs
@@ -93,12 +93,15 @@ namespace CineTicketHub.Migrations
                         .HasColumnType("int")
                         .HasColumnName("screening_id");
 
-                    b.Property<int>("UserId")
-                        .HasColumnType("int")
+                    b.Property<string>("UserId")
+                        .IsRequired()
+                        .HasColumnType("varchar(255)")
                         .HasColumnName("user_id");
 
                     b.HasKey("Id")
                         .HasName("PRIMARY");
+
+                    b.HasIndex("UserId");
 
                     b.HasIndex(new[] { "ScreeningId" }, "screening_id");
 
@@ -218,6 +221,11 @@ namespace CineTicketHub.Migrations
                         .IsConcurrencyToken()
                         .HasColumnType("longtext");
 
+                    b.Property<string>("Discriminator")
+                        .IsRequired()
+                        .HasMaxLength(21)
+                        .HasColumnType("varchar(21)");
+
                     b.Property<string>("Email")
                         .HasMaxLength(256)
                         .HasColumnType("varchar(256)");
@@ -268,6 +276,10 @@ namespace CineTicketHub.Migrations
                         .HasDatabaseName("UserNameIndex");
 
                     b.ToTable("AspNetUsers", (string)null);
+
+                    b.HasDiscriminator<string>("Discriminator").HasValue("IdentityUser");
+
+                    b.UseTphMappingStrategy();
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserClaim<string>", b =>
@@ -368,6 +380,13 @@ namespace CineTicketHub.Migrations
                     b.ToTable("movie_has_genre", (string)null);
                 });
 
+            modelBuilder.Entity("CineTicketHub.Models.Entities.ApplicationUser", b =>
+                {
+                    b.HasBaseType("Microsoft.AspNetCore.Identity.IdentityUser");
+
+                    b.HasDiscriminator().HasValue("ApplicationUser");
+                });
+
             modelBuilder.Entity("CineTicketHub.Models.Entities.Reservation", b =>
                 {
                     b.HasOne("CineTicketHub.Models.Entities.Screening", "Screening")
@@ -376,7 +395,15 @@ namespace CineTicketHub.Migrations
                         .IsRequired()
                         .HasConstraintName("reservations_ibfk_1");
 
+                    b.HasOne("CineTicketHub.Models.Entities.ApplicationUser", "User")
+                        .WithMany("Reservations")
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
                     b.Navigation("Screening");
+
+                    b.Navigation("User");
                 });
 
             modelBuilder.Entity("CineTicketHub.Models.Entities.Screening", b =>
@@ -477,6 +504,11 @@ namespace CineTicketHub.Migrations
                 });
 
             modelBuilder.Entity("CineTicketHub.Models.Entities.Screening", b =>
+                {
+                    b.Navigation("Reservations");
+                });
+
+            modelBuilder.Entity("CineTicketHub.Models.Entities.ApplicationUser", b =>
                 {
                     b.Navigation("Reservations");
                 });
