e3c4077cd903bcee262410bf6d9e1493853d109c
Anthony Roussos <github.eloquent385@passinbox.com>
2024-01-27 16:54:20 +0200

    Refine controllers

--- CineTicketHub/Controllers/ReservationsController.cs
+++ CineTicketHub/Controllers/ReservationsController.cs
@@ -1,29 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using CineTicketHub.Enums;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CineTicketHub.Models;
 using CineTicketHub.Models.Entities;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 
 namespace CineTicketHub.Controllers
 {
     public class ReservationsController : Controller
     {
         private readonly CineTicketHubContext _context;
-        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ReservationsController(CineTicketHubContext context, 
-            UserManager<ApplicationUser> userManager)
+        public ReservationsController(CineTicketHubContext context)
         {
             _context = context;
-            _userManager = userManager;
         }
 
         // GET: Reservations
@@ -39,6 +30,7 @@ namespace CineTicketHub.Controllers
                     .Include(r => r.User)
                     .Include(r => r.Screening.Movie)
                     .Include(r => r.Screening.Room)
+                    .OrderByDescending(r => r.Screening.StartsAt)
                     .ToList();
             }
             else
@@ -48,106 +40,11 @@ namespace CineTicketHub.Controllers
                     .Where(r => r.UserId == userId)
                     .Include(r => r.Screening.Movie)
                     .Include(r => r.Screening.Room)
+                    .OrderByDescending(r => r.Screening.StartsAt)
                     .ToList();
             }
             
             return View(reservations);
-        }
-
-        // GET: Reservations/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var reservation = await _context.Reservations
-                .Include(r => r.Screening)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (reservation == null)
-            {
-                return NotFound();
-            }
-
-            return View(reservation);
-        }
-
-        // GET: Reservations/Create
-        public IActionResult Create()
-        {
-            ViewData["ScreeningId"] = new SelectList(_context.Screenings, "Id", "Id");
-            return View();
-        }
-
-        // POST: Reservations/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,ScreeningId,UserId,NumOfSeats")] Reservation reservation)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(reservation);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ScreeningId"] = new SelectList(_context.Screenings, "Id", "Id", reservation.ScreeningId);
-            return View(reservation);
-        }
-
-        // GET: Reservations/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var reservation = await _context.Reservations.FindAsync(id);
-            if (reservation == null)
-            {
-                return NotFound();
-            }
-            ViewData["ScreeningId"] = new SelectList(_context.Screenings, "Id", "Id", reservation.ScreeningId);
-            return View(reservation);
-        }
-
-        // POST: Reservations/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ScreeningId,UserId,NumOfSeats")] Reservation reservation)
-        {
-            if (id != reservation.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(reservation);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ReservationExists(reservation.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["ScreeningId"] = new SelectList(_context.Screenings, "Id", "Id", reservation.ScreeningId);
-            return View(reservation);
         }
 
         // GET: Reservations/Delete/5
@@ -185,10 +82,6 @@ namespace CineTicketHub.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-
-        private bool ReservationExists(int id)
-        {
-            return _context.Reservations.Any(e => e.Id == id);
-        }
+        
     }
 }
